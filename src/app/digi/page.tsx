a998a11ddd2a92199bf9af39b282ac3a0c9d188b
Wakanyaben254 <86914451+Wakanyaben254@users.noreply.github.com>
2025-07-03 19:25:18 +0300

    Add files via upload
    impacts card route

---
+++ src/app/digi/page.tsx
@@ -0,0 +1,10 @@
+import DigiGates from "../components/DigiGates";
+export default function LearnPage() {
+  return (
+    <main className="min-h-screen">
+        <DigiGates />
+        {/* You can add a footer or any other components here if needed */}
+
+    </main>
+  );
+}
---
+++ src/app/wom/page.tsx
@@ -0,0 +1,10 @@
+import WomGates from "../components/WomGates";
+export default function WomPage() {
+  return (
+    <main className="min-h-screen">
+        <WomGates />
+        
+
+    </main>
+  );
+}
